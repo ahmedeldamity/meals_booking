8c13d674f200aa02eb184651269fb5dade4cc60b
ahmedeldamity <ahmedeldamity25@gmail.com>
2024-02-07 19:08:15 +0200

    meals-booking-project

---
+++ meals_booking/DTO/AppUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTO
+{
+    public class AppUserDto
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Token { get; set; }
+        public IEnumerable<string> Role { get; set; }
+    }
+}
---
+++ meals_booking/DTO/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTO
+{
+    public class RegisterDto
+    {
+        public string SSN { get; set; }
+
+        public string DisplayName { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        [Compare(nameof(Password))]
+        public string ConfirmPassword { get; set; }
+    }
+}
